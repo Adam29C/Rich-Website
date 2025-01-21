2fda91cb714dfffe6b89d4e5b18bfe454b6ed504
Adam29C <adamcatholic29@gmail.com>
2025-01-21 18:07:14 +0530

    update base url

--- src/components/Config/config.js
+++ src/components/Config/config.js
@@ -1,4 +1,5 @@
 // export const base_url = "http://192.168.1.12:6999/"
 // export const base_url = "http://3.10.226.224:5200/"
-export const base_url = "http://35.181.15.128:6999/"
+// export const base_url = "http://35.181.15.128:6999/"
+export const base_url = "https://connect.rich143.com/"
 
